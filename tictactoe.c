8b2e052146415507dbc499dda9078a9ce7303348
Pedro Henrique Francisco Pereira <pedrohenriquefp_2013@hotmail.com>
2026-04-18 23:41:48 -0300

    feat: add asking for first player choice and setup project

--- tictactoe.c
+++ tictactoe.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+
+void choiceOfXOrCircle();
+
+char firstPlayerChoice[10];
+char secondPlayerChoice[10];
 
 int main() {
-    printf("Hello World\n");
+    choiceOfXOrCircle();
     return 0;
+}
+
+void choiceOfXOrCircle() 
+{
+    printf("First player: X or O?\n");
+
+    if(fgets(firstPlayerChoice, sizeof firstPlayerChoice, stdin) != NULL) 
+    {
+        size_t len = strlen(firstPlayerChoice);
+        if (len > 0 && firstPlayerChoice[len-1] == '\n') firstPlayerChoice[len-1] = '\0'; // remove '\n'
+
+        if(firstPlayerChoice[0] == 'X' || firstPlayerChoice[0] == 'O')
+        {
+            printf("You typed: %s\n", firstPlayerChoice);
+        } else {
+            printf("Invalid option. Type X or 0\n");
+            choiceOfXOrCircle();
+        }  
+    }
 }
