7b5bde1cd8d2cb48fd47e8953ad0773fcfa6bb21
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2026-04-30 18:47:54 -0300

    chore: adding useful comments on request choice flow

--- tictactoe.c
+++ tictactoe.c
@@ -7,27 +7,28 @@ void printPlayerChoices();
 char firstPlayerChoice[10];
 char secondPlayerChoice[10];
 
-int main() 
-{
+int main() {
     choiceOfXOrCircle();
     return 0;
 }
 
-void choiceOfXOrCircle() 
-{
+void choiceOfXOrCircle() {
     printf("First player: X or O?\n");
 
-    if(fgets(firstPlayerChoice, sizeof firstPlayerChoice, stdin) != NULL) 
-    {
+    // Read what the user input on terminal
+    if (fgets(firstPlayerChoice, sizeof firstPlayerChoice, stdin) != NULL) {
+
+        // Get the length from input of the user
         size_t len = strlen(firstPlayerChoice);
-        if (len > 0 && firstPlayerChoice[len-1] == '\n') firstPlayerChoice[len-1] = '\0'; // remove '\n'
-        
-        if(firstPlayerChoice[0] == 'X' || firstPlayerChoice[0] == 'x') 
-        {
+
+        // Verify data is bigger than zero and if the final has \n
+        if (len > 0 && firstPlayerChoice[len - 1] == '\n')
+            firstPlayerChoice[len - 1] = '\0'; // remove '\n'
+
+        if (firstPlayerChoice[0] == 'X' || firstPlayerChoice[0] == 'x') {
             secondPlayerChoice[0] = 'O';
             printPlayerChoices();
-        } else if(firstPlayerChoice[0] == 'O' || firstPlayerChoice[0] == 'o') 
-        {   
+        } else if (firstPlayerChoice[0] == 'O' || firstPlayerChoice[0] == 'o') {
             secondPlayerChoice[0] = 'X';
             printPlayerChoices();
         } else {
@@ -37,8 +38,7 @@ void choiceOfXOrCircle()
     }
 }
 
-void printPlayerChoices() 
-{
+void printPlayerChoices() {
     printf("First player choice is: %s\n", firstPlayerChoice);
     printf("Second player choices is: %s\n", secondPlayerChoice);
 }
